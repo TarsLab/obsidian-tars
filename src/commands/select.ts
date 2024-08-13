281b4da52b81fa5ceef0463b20643a56808b58f1
C Jack <ae86jack@gmail.com>
2024-08-13 09:49:30 +0800

    feat: Add select message at cursor command

---
+++ src/commands/select.ts
@@ -0,0 +1,19 @@
+import { App, Command, Editor, MarkdownView, Notice } from 'obsidian'
+import { buildRunEnv, getMsgPositionByLine } from 'src/editor'
+import { t } from 'src/lang/helper'
+import { PluginSettings } from 'src/settings'
+
+export const selectMsgAtCursorCmd = (app: App, settings: PluginSettings): Command => ({
+	id: 'select-message-at-cursor',
+	name: t('Select message at cursor'),
+	editorCallback: async (editor: Editor, view: MarkdownView) => {
+		const env = await buildRunEnv(app, settings)
+		const currentLine = editor.getCursor('to').line
+		const [startOffset, endOffset] = getMsgPositionByLine(env, currentLine)
+		if (startOffset === -1 || endOffset === -1) {
+			new Notice(t('No message found at cursor'))
+			return
+		}
+		editor.setSelection(editor.offsetToPos(startOffset), editor.offsetToPos(endOffset))
+	}
+})
