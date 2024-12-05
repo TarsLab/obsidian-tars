ab7c3c43f56f70c7844aa8bf493a2b309a9cefeb
C Jack <ae86jack@gmail.com>
2024-12-05 16:49:52 +0800

    Add Gemini vendor

---
+++ src/providers/gemini.ts
@@ -0,0 +1,45 @@
+import { Content, GoogleGenerativeAI } from '@google/generative-ai'
+import { t } from 'src/lang/helper'
+import { BaseOptions, Message, SendRequest, Vendor } from '.'
+
+const sendRequestFunc = (settings: BaseOptions): SendRequest =>
+	async function* (messages: Message[]) {
+		const { parameters, ...optionsExcludingParams } = settings
+		const options = { ...optionsExcludingParams, ...parameters } // 这样的设计，让parameters 可以覆盖掉前面的设置 optionsExcludingParams
+		const { apiKey, baseURL: baseUrl, model } = options
+		if (!apiKey) throw new Error(t('API key is required'))
+
+		const [system_msg, messagesWithoutSys, lastMsg] =
+			messages[0].role === 'system'
+				? [messages[0], messages.slice(1, -1), messages[messages.length - 1]]
+				: [null, messages.slice(0, -1), messages[messages.length - 1]]
+		const systemInstruction = system_msg?.content
+		const history: Content[] = messagesWithoutSys.map((m) => ({
+			role: m.role === 'assistant' ? 'model' : m.role,
+			parts: [{ text: m.content }]
+		}))
+
+		const genAI = new GoogleGenerativeAI(apiKey)
+		const genModel = genAI.getGenerativeModel({ model, systemInstruction }, { baseUrl })
+		const chat = genModel.startChat({ history })
+
+		const result = await chat.sendMessageStream(lastMsg.content)
+		for await (const chunk of result.stream) {
+			const chunkText = chunk.text()
+			// console.debug('chunkText', chunkText)
+			yield chunkText
+		}
+	}
+
+export const geminiVendor: Vendor = {
+	name: 'Gemini',
+	defaultOptions: {
+		apiKey: '',
+		baseURL: 'https://generativelanguage.googleapis.com',
+		model: 'gemini-1.5-flash',
+		parameters: {}
+	},
+	sendRequestFunc,
+	models: [],
+	websiteToObtainKey: 'https://makersuite.google.com/app/apikey'
+}
