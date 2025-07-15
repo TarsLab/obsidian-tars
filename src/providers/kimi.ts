03b6dc5e78673fb33987f5bc6554835d2af6ce9f
C Jack <ae86jack@gmail.com>
2025-07-15 16:19:13 +0800

    feat: enhance sendRequestFunc to format messages and handle reasoning content

--- src/providers/kimi.ts
+++ src/providers/kimi.ts
@@ -1,18 +1,20 @@
 import axios from 'axios'
 import { t } from 'src/lang/helper'
 import { BaseOptions, Message, ResolveEmbedAsBinary, SendRequest, Vendor } from '.'
+import { CALLOUT_BLOCK_END, CALLOUT_BLOCK_START, convertEmbedToImageUrl } from './utils'
 
 const sendRequestFunc = (settings: BaseOptions): SendRequest =>
-	async function* (messages: Message[], controller: AbortController, _resolveEmbedAsBinary: ResolveEmbedAsBinary) {
+	async function* (messages: Message[], controller: AbortController, resolveEmbedAsBinary: ResolveEmbedAsBinary) {
 		const { parameters, ...optionsExcludingParams } = settings
 		const options = { ...optionsExcludingParams, ...parameters }
 		const { apiKey, baseURL, model, ...remains } = options
 		if (!apiKey) throw new Error(t('API key is required'))
 		if (!model) throw new Error(t('Model is required'))
 
+		const formattedMessages = await Promise.all(messages.map((msg) => formatMsg(msg, resolveEmbedAsBinary)))
 		const data = {
 			model,
-			messages,
+			messages: formattedMessages,
 			stream: true,
 			...remains
 		}
@@ -30,6 +32,7 @@ const sendRequestFunc = (settings: BaseOptions): SendRequest =>
 		const reader = response.data.pipeThrough(new TextDecoderStream()).getReader()
 
 		let reading = true
+		let startReasoning = false
 		while (reading) {
 			const { done, value } = await reader.read()
 			if (done) {
@@ -48,16 +51,59 @@ const sendRequestFunc = (settings: BaseOptions): SendRequest =>
 				const trimmedPart = part.replace(/^data: /, '').trim()
 				if (trimmedPart) {
 					const data = JSON.parse(trimmedPart)
-					if (data.choices && data.choices[0].delta && data.choices[0].delta.content) {
-						const content = data.choices[0].delta.content
-						yield content
+					if (data.choices && data.choices[0].delta) {
+						const delta = data.choices[0].delta
+						const reasonContent = delta.reasoning_content
+
+						if (reasonContent) {
+							const prefix = !startReasoning ? ((startReasoning = true), CALLOUT_BLOCK_START) : ''
+							yield prefix + reasonContent.replace(/\n/g, '\n> ') // Each line of the callout needs to have '>' at the beginning
+						} else {
+							const prefix = startReasoning ? ((startReasoning = false), CALLOUT_BLOCK_END) : ''
+							if (delta.content) {
+								yield prefix + delta.content
+							}
+						}
 					}
 				}
 			}
 		}
 	}
 
-const models = ['moonshot-v1-128k', 'moonshot-v1-32k', 'moonshot-v1-8k']
+type ContentItem =
+	| {
+			type: 'image_url'
+			image_url: {
+				url: string
+			}
+	  }
+	| { type: 'text'; text: string }
+
+const formatMsg = async (msg: Message, resolveEmbedAsBinary: ResolveEmbedAsBinary) => {
+	const content: ContentItem[] = msg.embeds
+		? await Promise.all(msg.embeds.map((embed) => convertEmbedToImageUrl(embed, resolveEmbedAsBinary)))
+		: []
+
+	if (msg.content.trim()) {
+		content.push({
+			type: 'text' as const,
+			text: msg.content
+		})
+	}
+	return {
+		role: msg.role,
+		content
+	}
+}
+
+const models = [
+	'moonshot-v1-128k',
+	'moonshot-v1-32k',
+	'moonshot-v1-32k-vision-preview',
+	'kimi-thinking-preview',
+	'kimi-k2-0711-preview',
+	'kimi-latest-32k'
+]
 
 export const kimiVendor: Vendor = {
 	name: 'Kimi',
@@ -70,5 +116,5 @@ export const kimiVendor: Vendor = {
 	sendRequestFunc,
 	models,
 	websiteToObtainKey: 'https://www.moonshot.cn',
-	capabilities: ['Text Generation']
+	capabilities: ['Text Generation', 'Image Vision', 'Reasoning']
 }
